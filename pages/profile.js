bd434e3fa9379e728097d529e1d382d04e3d1d04
ali-pixel38 <ali@pixel38.com>
2022-02-25 00:02:57 +0200

    229 done

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,23 @@
+import { getSession } from 'next-auth/client';
+
 import UserProfile from '../components/profile/user-profile';
 
 function ProfilePage() {
   return <UserProfile />;
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req })
+  if (!session)
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false //only this time we need to redirect
+      }
+    }
+  return {
+    props: { session }
+  }
 }
 
 export default ProfilePage;
